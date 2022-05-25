600662ff963a808d7817c10042ea26dad6afd8d7
Andre Carlos <amarques@a5solutions.com>
2022-05-25 00:47:21 -0300

    Form simples, treinamento de useState

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,23 +1,40 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom"
 
 
 
 export const Login = () => {
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     const navigate = useNavigate();
 
 
-    const handleClick = () => {
-        navigate('/dashboard');
+    const handleEntrar = () => {
+        console.log(email, password);
     }
 
     return (
         <div>
-            <div>
-                Login
-            </div>
+            <form>
+                <div>
+                    <label>
+                        <span>Email</span>
+                        <input value={email} onChange={e => setEmail(e.target.value)} type="text" />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <span>Senha</span>
+                        <input value={password} onChange={e => setPassword(e.target.value)} type="password" />
+                    </label>
+                </div>
 
-            <button onClick={handleClick}>Dashboard</button>
+                <button type="button" onClick={handleEntrar}>
+                    Entrar
+                </button>
+            </form>
         </div>
     )
 }

02d72cefc210b0baea907ff74df7af2f730d5bf8
Andre Carlos <amarques@a5solutions.com>
2022-05-30 22:46:08 -0300

    config Json-server, teste API

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import { Dashboard, Login } from "../pages"
+import { Lista } from "../pages/lista/Lista"
 
 export const Rota = () => {
     return (
@@ -7,6 +8,7 @@ export const Rota = () => {
             <Routes>
                 <Route path="/login" element={<Login/>} />
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/lista" element={<Lista />} />
 
                 <Route path="*" element={<Navigate to="/dashboard" />} />
             </Routes>

b68ab1ee9771334e2e7649152b703c4837bb9e3d
Andre Carlos <amarques@a5solutions.com>
2022-06-04 01:32:47 -0300

    Documentando funcionalidade

--- src/app/shared/services/storage/useStorage.js
+++ src/app/shared/services/storage/useStorage.js
@@ -1,6 +1,11 @@
 import { useCallback, useState } from 'react';
 import storage from './storage';
 
+/**
+ * Hook personalizado para funcionar semelhante ao useState
+ * porem, ela armazena o valor setado tbm no storage do browser
+ * assim o valor da variavel não se perde ao fechar browser ou apos refresh 
+ */
 export default function useStorage(key) {
   const [state, setState] = useState(() => storage.get(key));
 

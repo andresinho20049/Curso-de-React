dab540a947be59e115a3f8c89556b6fcd401b7cd
Andre Carlos <amarques@a5solutions.com>
2022-06-06 19:44:25 -0300

    Criando Form com Unform e finalizando CRUD Pessoa

--- src/app/pages/pessoas/DetalhePessoas.tsx
+++ src/app/pages/pessoas/DetalhePessoas.tsx
@@ -1,21 +1,33 @@
-import { LinearProgress } from "@mui/material";
-import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Box, Grid, LinearProgress, Paper, Typography } from "@mui/material";
+import { FormHandles } from "@unform/core";
+import { Form } from "@unform/web";
+
 import { FerramentasDetalhe } from "../../shared/components";
+import { VTextField } from "../../shared/components/forms";
 import { LayoutBasePaginas } from "../../shared/layout";
 import { PessoasService } from "../../shared/services";
 
+interface IFormData {
+    nomeCompleto: string;
+    email: string;
+    cidadeId: number;
+}
 
 export const DetalhePessoas = () => {
 
+    var isSaveAndClose = false;
     const { id } = useParams<'id'>();
     const navigate = useNavigate();
+
+    const formRef = useRef<FormHandles>(null);
 
     const [isLoading, setIsLoading] = useState(false);
     const [nome, setNome] = useState('');
 
     useEffect(() => {
-
+        console.log(id);
         if (id !== undefined) {
             setIsLoading(true);
             PessoasService.getById(Number(id))
@@ -24,23 +36,61 @@ export const DetalhePessoas = () => {
                         alert(result.message);
                         navigate('/pessoas');
                     } else {
-                        console.log(result);
                         setNome(result.nomeCompleto);
+
+                        formRef.current?.setData(result);
+                    }
+                    setIsLoading(false);
+                })
+        } else {
+            formRef.current?.setData({
+                nomeCompleto: '',
+                email: '',
+                cidadeId: ''
+            })
+        }
+    }, [id])
+
+    const handleSave = (dados: IFormData) => {
+
+        setIsLoading(true);
+        if (id === undefined) {
+            PessoasService.create(dados)
+                .then((result) => {
+                    if (result instanceof Error) {
+                        alert(result.message);
+                    } else {
+                        if(isSaveAndClose){
+                            navigate('/pessoas')
+                        } else {
+                            navigate(`/pessoas/detalhe/${result}`)
+                        }
+                    }
+                    setIsLoading(false)
+                })
+        } else {
+            PessoasService.update({ id: Number(id), ...dados })
+                .then((result) => {
+                    if (result instanceof Error) {
+                        alert(result.message);
+                    } else {
+                        if(isSaveAndClose){
+                            navigate('/pessoas');
+                        } else {
+                            setNome(dados.nomeCompleto);
+                        }
                     }
                     setIsLoading(false);
                 })
         }
-    }, [])
 
-    const handleSave = () => {
-        console.log('Save')
     }
 
     const handleDelete = () => {
-        if(window.confirm('Deseja excluir realmente?')){
+        if (window.confirm(`Deseja excluir realmente? ${nome}`)) {
             PessoasService.deleteById(Number(id))
                 .then(result => {
-                    if(result instanceof Error){
+                    if (result instanceof Error) {
                         alert(result.message)
                         // navigate('/pessoas')
                     } else {
@@ -49,10 +99,6 @@ export const DetalhePessoas = () => {
                     }
                 })
         }
-    }
-
-    const handleSaveAndBack = () => {
-        console.log('Salvar e voltar');
     }
 
     return (
@@ -64,18 +110,67 @@ export const DetalhePessoas = () => {
                     isVisibleBtnSalvaVoltar
                     isVisibleBtnNovo={id !== undefined}
                     isVisibleBtnApagar={id !== undefined}
+                    isLoading={isLoading}
 
-                    clickSalvar={handleSave}
+                    clickSalvar={() => {isSaveAndClose = false; formRef.current?.submitForm();}}
                     clickApagar={handleDelete}
-                    clickSalvarVoltar={handleSaveAndBack}
+                    clickSalvarVoltar={() => {isSaveAndClose = true; formRef.current?.submitForm();}}
                     clickVoltar={() => navigate('/pessoas')}
                     clickNovo={() => navigate('/pessoas/detalhe')}
                 />}>
 
-            {isLoading && (
-                <LinearProgress variant="indeterminate" />
-            )}
 
-        </LayoutBasePaginas>
+
+            <Form ref={formRef} onSubmit={handleSave}>
+
+                <Box margin={1} display='flex' flexDirection='column' component={Paper} variant='outlined'>
+
+                    <Grid container direction='column' padding={2} spacing={2}>
+
+                        <Grid item>
+                            {isLoading && (
+                                <LinearProgress variant="indeterminate" />
+                            )}
+                        </Grid>
+
+                        <Grid item>
+                            <Typography variant="h6">Geral</Typography>
+                        </Grid>
+
+                        <Grid container item direction='row' spacing={2} >
+                            <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
+                                <VTextField
+                                    fullWidth
+                                    name="nomeCompleto"
+                                    label="Nome"
+                                    disabled={isLoading}
+                                />
+                            </Grid>
+                        </Grid>
+                        <Grid container item direction='row'>
+                            <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
+                                <VTextField
+                                    fullWidth
+                                    name="email"
+                                    label="E-mail"
+                                    disabled={isLoading}
+                                />
+                            </Grid>
+                        </Grid>
+                        <Grid container item direction='row'>
+                            <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
+                                <VTextField
+                                    fullWidth
+                                    name="cidadeId"
+                                    label="Cidade"
+                                    disabled={isLoading}
+                                />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Form>
+
+        </LayoutBasePaginas >
     )
 }
